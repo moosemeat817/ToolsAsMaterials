8540091bd29716f0db48309dfa4c85e2676783b2
moosemeat817 <michaelsd123456@gmail.com>
2025-04-13 13:59:42 -0500

    Add files via upload

---
+++ Patches.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Il2Cpp;
+using HarmonyLib;
+using MelonLoader;
+using Il2CppTLD.Gear;
+
+namespace ToolsAsMaterials
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(GearItem), nameof(GearItem.Awake))]
+        public class AddToolsToMaterialsFilter
+        {
+            public static void Postfix(GearItem __instance)
+            {
+                // Normalize the name to strip "(Clone)" and whitespace
+                string cleanName = __instance.name.Replace("(Clone)", "").Trim();
+
+                HashSet<string> toolNames = new()
+            {
+                "GEAR_Line",
+                "GEAR_Hook",
+                "GEAR_ArrowHead",
+                "GEAR_ArrowShaft",
+                "GEAR_RevolverAmmoCasing",
+                "GEAR_RifleAmmoCasing"
+            };
+
+                //MelonLogger.Msg($"[ToolsAsMaterials] Checking gear name: {cleanName}");
+
+                if (toolNames.Contains(cleanName))
+                {
+                    __instance.GearItemData.m_Type = GearType.Material;
+                    //MelonLogger.Msg($"[ToolsAsMaterials] Converted {cleanName} to Material");
+                }
+            }
+        }
+    }
+
+}
---
+++ ToolsAsMaterials.cs
@@ -0,0 +1,17 @@
+using MelonLoader;
+using MelonLoader.Utils;
+
+namespace ToolsAsMaterials
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon() 
+        {
+
+            Debug.Log($"[{Info.Name}] Version {Info.Version} loaded!");
+
+        }
+
+
+    }
+}
